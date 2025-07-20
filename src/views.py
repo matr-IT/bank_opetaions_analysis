2abccfaec2bee913a9b716e4df0ef6fb22cb998c
rybin <rybin.32@gmail.com>
2025-07-20 22:34:04 +0300

    load_transactions and home page funcs init

--- src/views.py
+++ src/views.py
@@ -1,18 +1,34 @@
-import datetime
-import json
-import requests
-import logging
+import pandas as pd
 
 
+def load_transactions(file_path: str) -> pd.DataFrame:
+    """Читает Excel-файл и выдает DataFrame"""
+    try:
+        df = pd.read_excel(file_path)
+        return df
+    except ValueError as ex:
+        print(ex)
+    except FileNotFoundError as ex:
+        print(ex)
 
-def greeting():
-    current_hour = datetime.datetime.now().hour
-    """На основе текущего времени функция возвращает приветствие"""
-    if 5 <= current_hour < 12:
-        return "Доброе утро"
-    elif 12 <= current_hour < 18:
-        return "Добрый день"
-    elif 18 <= current_hour < 23:
-        return "Добрый вечер"
-    else:
-        return "Доброй ночи"
+
+def home_page(date_time: str) -> list[dict]:
+    """Функция отображает данные по картам: номер, сумму трат по карте и полученный кэшбэк"""
+    date_time_pd = pd.to_datetime(date_time)
+    operations = load_transactions('/Users/rybin/PycharmProjects/bank_operations_analysis/data/operations.xlsx')
+    operations['Дата операции'] = pd.to_datetime(operations['Дата операции'])
+    first_day_of_month = date_time_pd.replace(day=1)
+    filtered = operations[operations['Дата операции']] >= first_day_of_month
+    filtered = filtered[filtered['Дата операции']] <= date_time_pd
+
+    card_data = []
+    for card in filtered['Номер карты'].dropna().unique():
+        card_transactions = filtered[filtered['Номер карты'] == card]
+        total_spent = sum([i for i in card_transactions['Сумма операции'] if i < 0])
+        card_data.append({
+            "last_digits": card[-4:],
+            "total_spent": float(total_spent),
+            "cashback": round(float(total_spent * 0.01), 2)
+        })
+    return card_data
+
