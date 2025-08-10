adb0ce7da351e25c55386eca6f0aee8c6042f82e
rybin <rybin.32@gmail.com>
2025-08-10 21:19:44 +0300

    small fixes in services.py

--- src/services.py
+++ src/services.py
@@ -19,13 +19,13 @@ file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(me
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
+operations = load_transactions(
+        "/Users/rybin/PycharmProjects/bank_operations_analysis/data/operations.xlsx"
+    ).to_dict(orient="records")
 
 def process_bank_search(search=None) -> json:
     """Фильтрует операции по ключевым словам из описания операции или категории"""
-    logger.info("Загружаем список операций")
-    operations = load_transactions(
-        "/Users/rybin/PycharmProjects/bank_operations_analysis/data/operations.xlsx"
-    ).to_dict(orient="records")
+
     logger.info("Проверяем наличие ключевых слов для поиска")
     if not search:
         return operations
